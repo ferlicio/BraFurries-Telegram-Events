be94cd2240fd93b18141231f97b5c47ae54d7e6d
Fernando FR <ferlicio@gmail.com>
2024-04-20 02:25:42 -0300

    adequacao para projeto isolado

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from message_services.telegram.cron_jobs import startEventUpdater
 from database.database import *
-from message_services.telegram.telegram_service import run_telegram_client
 import sys, codecs
 import threading
 
@@ -11,12 +10,10 @@ chatBot = {
 }
 
 
-getCredentials()
 
 
 threads = []
 threads.append(threading.Thread(target=startEventUpdater))
-#run_telegram_client()
 
 for thread in threads:
     thread.daemon = True
